cb9178254788d5b0b302925b7bcdf5ce6cd8213f
zor1497 <serezhenka.zorin.97@gmail.com>
2024-05-28 20:40:30 +0700

    second commit

--- conftest.py
+++ conftest.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 import pytest
-
-
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser():
-    browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    browser = webdriver.Chrome(options=options)
     browser.maximize_window()
     yield browser
     browser.quit()
