823d0cb93c799c3ceff25b8dc73487020e565432
zor1497 <serezhenka.zorin.97@gmail.com>
2024-05-30 23:05:40 +0700

    add marks and all tests option

--- test_about_page.py
+++ test_about_page.py
@@ -1,11 +1,10 @@
-import time
-
 import allure
 from pages.about_page import AboutPage
 from pages.login_page import LoginPage
+import pytest
 
 
-
+@pytest.mark.smoke
 @allure.title("Открытие страницы регистрации со страницы 'О Вконтакте'")
 def test_open_login_page_from_about_page(browser):
     about_page = AboutPage(browser)
--- test_login_page.py
+++ test_login_page.py
@@ -1,14 +1,17 @@
 import allure
 from pages.about_page import AboutPage
 from pages.login_page import LoginPage
+import pytest
 
-
+@pytest.mark.smoke
 @allure.title("Открытие страницы авторизации")
 def test_open_login_page(browser):
     login_page = LoginPage(browser)
     login_page.open()
     login_page.is_opened()
 
+
+@pytest.mark.regress
 @allure.title("Открытие страницы 'О Вконтакте' со страницы авторизации")
 def test_open_about_page_from_login_page(browser):
     login_page = LoginPage(browser)
