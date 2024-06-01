8fb638b07b888ddbfcacec83e6a424cb094302de
zor1497 <serezhenka.zorin.97@gmail.com>
2024-06-01 13:45:45 +0700

    delete language test

--- pages/login_page.py
+++ pages/login_page.py
@@ -11,15 +11,6 @@ class LoginPage(BasePage):
     RULES_LINK = ("xpath", "(//a[@href='/terms'])[1]")
     CREATE_ACC_BUTTON = ("xpath", "//span[text()='Создать аккаунт']")
     FOR_DEVELOPERS_LINK = ("xpath", "(//a[@href='https://dev.vk.com'])[1]")
-    LANGUAGE_LIST = ("id", "all_languages_list")
-    CLOSE_LANGUAGE_LIST_BUTTON = ("xpath", "//div[@class='box_controls_wrap']//button")
-    LANGUAGE_SEARCH_FROM = ("id", "language_search_form")
-    LANGUAGE_SEARCH_BUTTON = ("xpath", "(//div[@class='ui_search_input_block']//button)[1]")
-    LANGUAGE_LINK = ("xpath", "(//div[@class='footer_lang'])[1]//a[3]")
-    LANGUAGES_IN_FIRST_COLUMN = ("xpath", "//div[@class='language_column fl_l']//a")
-    LOGIN_MOBILE_HEADER = ("xpath", "//div[@class='login_mobile_header']")
-    LANGUAGE_DEFAULT_LINK = ("xpath", "(//div[@class='footer_lang'])[1]//a[1]")
-
 
     @allure.step("Нажатие на ссылку 'О Вконтакте'")
     def click_on_about_vk_link(self):
@@ -28,33 +19,6 @@ class LoginPage(BasePage):
     @allure.step("Нажатие на ссылку 'Разработчикам'")
     def click_on_for_developers_link(self):
         self.wait.until(EC.visibility_of_element_located(self.FOR_DEVELOPERS_LINK)).click()
-
-    @allure.step("Нажатие на ссылку 'Все языки'")
-    def click_on_language_link(self):
-        self.wait.until(EC.element_to_be_clickable(self.LANGUAGE_LINK)).click()
-        self.wait.until(EC.visibility_of_element_located(self.LANGUAGE_LIST))
-
-    def type_text_into_language_search_form(self, text):
-        with allure.step(f"Ввод текса {text} в поле поиска языков"):
-            language_search_form = self.wait.until(EC.visibility_of_element_located(self.LANGUAGE_SEARCH_FROM))
-            language_search_form.send_keys(text)
-            assert language_search_form.get_attribute("value") == text
-
-    @allure.step("Нажатие на ссылку английского языка")
-    def click_on_english_language(self):
-        english_language = ("xpath", "//span[text()='English']")
-        self.wait.until(EC.element_to_be_clickable(english_language)).click()
-
-    @allure.step("Заголовок страницы авторизации описывается на английском языке")
-    def should_be_english_version(self):
-        print(self.wait.until(EC.visibility_of_element_located(self.LOGIN_MOBILE_HEADER)).text)
-        assert self.wait.until(EC.visibility_of_element_located(self.LOGIN_MOBILE_HEADER)).text == "VK for mobile devices"
-
-
-
-
-        
-
 
 
 
