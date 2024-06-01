d7c6daaaa6c942cca452c0e346995500515d0c3c
zor1497 <serezhenka.zorin.97@gmail.com>
2024-06-01 13:36:37 +0700

    -

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 def browser():
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': 'ru'})
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
