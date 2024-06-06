9d3375132d3689c8bb59a37aa23dfb085d4f9628
zor1497 <serezhenka.zorin.97@gmail.com>
2024-06-06 14:24:31 +0700

    some refactoring + add sort tests

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,5 +1,6 @@
 import allure
 from pages.base_page import BasePage
+from selenium.webdriver.support.select import Select
 
 
 class InventoryPage(BasePage):
@@ -11,10 +12,11 @@ class InventoryPage(BasePage):
     ITEM_PRICE = ("xpath", ".//div[@data-test='inventory-item-price']")
     ADD_ITEM_TO_BASKET_BUTTON = ("xpath", ".//button[@id[contains(.,'add')]]")
     REMOVE_ITEM_FROM_BASKET_BUTTON = ("xpath", ".//button[@id[contains(.,'remove')]]")
+    PRODUCT_SORT_CONTAINER = ("xpath", "//select[@data-test='product-sort-container']")
 
     # ACTIONS
-    @allure.step("Добавление одного товара в корзину")
-    def add_one_item_into_basket(self):
+    @allure.step("Добавление товара в корзину")
+    def add_item_into_basket(self):
         item = {
             "name": self.find_element(self.ITEM).find_element(*self.ITEM_NAME).text,
             "desc": self.find_element(self.ITEM).find_element(*self.ITEM_DESC).text,
@@ -40,9 +42,54 @@ class InventoryPage(BasePage):
             items.append(item)
         return items
 
+    @allure.step("Открытие страницы товара")
+    def open_item_page(self):
+        item = self.find_element(self.ITEM)
+        item.find_element(*self.ITEM_NAME).click()
+        assert "item" in self.get_current_url()
 
 
+    @allure.step("Сортировка товаров по параметру")
+    def sort_items(self, sort_type):
+        sort_container = Select(self.find_element(self.PRODUCT_SORT_CONTAINER))
+        sort_container.select_by_value(sort_type)
 
+    # SHOULD BE
+    @allure.step("Проверка сортировки товаров по возрастанию цены")
+    def should_be_items_sort_low_high_price(self):
+        items_price = self.browser.find_elements(*self.ITEM_PRICE)
+        min_price = float(items_price[0].text[1:])
+        for item in items_price:
+            item_price = float(item.text[1:])
+            assert min_price <= item_price
+            min_price = item_price
+
+    @allure.step("Проверка сортировки товаров по убыванию цены")
+    def should_be_items_sort_high_low_price(self):
+        items_price = self.browser.find_elements(*self.ITEM_PRICE)
+        max_price = float(items_price[0].text[1:])
+        for item in items_price:
+            item_price = float(item.text[1:])
+            assert max_price >= item_price
+            max_price = item_price
+
+    @allure.step("Проверка сортировки товаров по возрастанию названия")
+    def should_be_items_sort_a_z(self):
+        items_name = self.browser.find_elements(*self.ITEM_NAME)
+        min_name = items_name[0].text.lower()
+        for item in items_name:
+            item_name = item.text.lower()
+            assert min_name <= item_name
+            min_name = item_name
+
+    @allure.step("Проверка сортировки товаров по убыванию названия")
+    def should_be_items_sort_z_a(self):
+        items_name = self.browser.find_elements(*self.ITEM_NAME)
+        max_name = items_name[0].text.lower()
+        for item in items_name:
+            item_name = item.text.lower()
+            assert max_name >= item_name
+            max_name = item_name
 
 
 
--- test_saucedemo.py
+++ test_saucedemo.py
@@ -38,19 +38,20 @@ def test_auth_with_locked_account(browser):
     main_page.should_be_error_about_locked_user()
 
 @pytest.mark.smoke
-@allure.title("Добавление одного товара в корзину")
+@allure.title("Добавление товара в корзину со страницы товара")
 def test_add_one_item_into_basket(browser):
     main_page = MainPage(browser)
     main_page.open()
     main_page.auth("standard_user", "secret_sauce")
     inventory_page = InventoryPage(browser)
-    item = inventory_page.add_one_item_into_basket()
+    inventory_page.open_item_page()
+    item = inventory_page.add_item_into_basket()
     inventory_page.open_basket()
     basket_page = BasketPage(browser)
     basket_page.should_be_item_in_basket(item)
 
 @pytest.mark.smoke
-@allure.title("Добавление всех товаров в корзину")
+@allure.title("Добавление товаров в корзину со страницы Inventory")
 def test_add_all_items_into_basket(browser):
     main_page = MainPage(browser)
     main_page.open()
@@ -60,3 +61,42 @@ def test_add_all_items_into_basket(browser):
     inventory_page.open_basket()
     basket_page = BasketPage(browser)
     basket_page.should_be_all_items_in_basket(items_list)
+
+@pytest.mark.regress
+@allure.title("Сортировка товаров по возрастанию цены")
+def test_sort_items_low_high_price(browser):
+    main_page = MainPage(browser)
+    main_page.open()
+    main_page.auth("standard_user", "secret_sauce")
+    inventory_page = InventoryPage(browser)
+    inventory_page.sort_items("lohi")
+    inventory_page.should_be_items_sort_low_high_price()
+
+@pytest.mark.regress
+@allure.title("Сортировка товаров по убыванию цены")
+def test_sort_items_high_low_price(browser):
+    main_page = MainPage(browser)
+    main_page.open()
+    main_page.auth("standard_user", "secret_sauce")
+    inventory_page = InventoryPage(browser)
+    inventory_page.sort_items("hilo")
+    inventory_page.should_be_items_sort_high_low_price()
+
+@allure.title("Сортировка товаров по возрастанию названия")
+def test_sort_items_a_z(browser):
+    main_page = MainPage(browser)
+    main_page.open()
+    main_page.auth("standard_user", "secret_sauce")
+    inventory_page = InventoryPage(browser)
+    inventory_page.sort_items("az")
+    inventory_page.should_be_items_sort_a_z()
+
+@pytest.mark.regress
+@allure.title("Сортировка товаров по убыванию названия")
+def test_sort_items_z_a(browser):
+    main_page = MainPage(browser)
+    main_page.open()
+    main_page.auth("standard_user", "secret_sauce")
+    inventory_page = InventoryPage(browser)
+    inventory_page.sort_items("za")
+    inventory_page.should_be_items_sort_z_a()
